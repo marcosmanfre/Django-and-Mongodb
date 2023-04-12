34aa051b963daab40542891ef01fc5bb4b27a68d
marcosmanfre <marcosmanfre@hotmail.com>
2023-04-12 20:23:17 -0300

    models

--- djangoapp/models.py
+++ djangoapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Alunos(models.Model):
+    id=models.ObjectIdField()
+    nome=models.CharField(max_length=150)
+    sobrenome=models.CharField(max_length=150)
+    secao=models.CharField(max_length=150)
